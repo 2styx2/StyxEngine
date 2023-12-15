010b5bcd9001f5fb5f060a530295e264c2186f00
2sytx2 <wcmatth3@ncsu.edu>
2023-12-15 02:47:43 -0500

    init commit

---
+++ src/main/java/com/styxdev/test/Launcher.java
@@ -0,0 +1,18 @@
+package com.styxdev.test;
+
+import com.styxdev.core.WindowManager;
+import org.lwjgl.Version;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("STYX ENGINE", 1600, 900, false);
+        window.init();
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
+    }
+}
